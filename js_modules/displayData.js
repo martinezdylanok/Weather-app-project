617bee6237a31549c1e680b298da4f102940ea9a
Lpik97 <dm97fx@gmail.com>
2023-09-07 20:09:27 +0200

    feat(weather card element selected, elements inner html modified)
    THe weather card element was selected to then add the active class when displayed. All the elements inside the weather card now have a custom message in their innerHTML.

--- js_modules/displayData.js
+++ js_modules/displayData.js
@@ -4,12 +4,14 @@ const DISPLAY_DATA = (DATA) => {
    const FEELS_LIKE = document.querySelector(".feels-like");
    const HUMIDTY = document.querySelector(".humidity");
    const WIND = document.querySelector(".wind");
+   const WEATHER_CARD = document.querySelector(".weather-card");
 
-   CITY.innerText = DATA.location.name;
-   CELCIUS.innerText = DATA.current.temp_c;
-   FEELS_LIKE.innerText = DATA.current.feelslike_c;
-   HUMIDTY.innerText = DATA.current.humidity;
-   WIND.innerText = DATA.current.wind_kph;
+   WEATHER_CARD.classList.add("active");
+   CITY.innerHTML = DATA.location.name;
+   CELCIUS.innerHTML = `${DATA.current.temp_c}  °C`;
+   FEELS_LIKE.innerHTML = `Feels like: ${DATA.current.feelslike_c} °C`;
+   HUMIDTY.innerHTML = `Humidity: ${DATA.current.humidity} %`;
+   WIND.innerHTML = `Wind: ${DATA.current.wind_kph} km/h`;
 };
 
 export default DISPLAY_DATA;
